4b17b378a9e665e7187c97fa7ec8422e27c0560a
Brian Igirisa <brian.igirisaa@gmail.com>
2025-06-17 23:26:59 -0400

    updating the migration for create user

--- backend/src/db/migrations/20250526235613-create-user.js
+++ backend/src/db/migrations/20250526235613-create-user.js
@@ -18,6 +18,10 @@ module.exports = {
       email: {
         type: Sequelize.STRING
       },
+      password: { // <--- ADDED THIS COLUMN
+        type: Sequelize.STRING,
+        allowNull: false // Passwords should be non-nullable after hashing
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
